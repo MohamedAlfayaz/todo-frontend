bc2a8d2d6f4d2ba9f71dfad5cd382f2bd07d2ca0
MohamedAlfayaz <alfayaz23112004@gmail.com>
2026-03-08 14:41:53 +0530

    Update API URL and enhance error handling

--- src/api/api.js
+++ src/api/api.js
@@ -1,35 +1,42 @@
-const BASE_URL = 'http://localhost:3000/api/tasks';
+const BASE_URL = "https://todo-backend-cik7.onrender.com/api/tasks";
 
 export const fetchTasks = async () => {
-    const res = await fetch(BASE_URL);
-    return res.json();
-}
+  const res = await fetch(BASE_URL);
+  if (!res.ok) throw new Error("Failed to fetch tasks");
+  return res.json();
+};
 
 export const addTask = async (task) => {
-    const res = await fetch(BASE_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ task })
-    });
-    return res.json();
-}
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ task }),
+  });
 
-export const updateTask = async ({id, task}) => {
-    const res = await fetch(`${BASE_URL}/${id}`,{
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ task })
-    });
-    return res.json();
-}
+  if (!res.ok) throw new Error("Failed to add task");
+  return res.json();
+};
+
+export const updateTask = async ({ id, task }) => {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ task }),
+  });
+
+  if (!res.ok) throw new Error("Failed to update task");
+  return res.json();
+};
 
 export const deleteTask = async (id) => {
-    const res = await fetch(`${BASE_URL}/${id}`, {
-        method: "DELETE",
-    });
-    return res.json();
-}
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) throw new Error("Failed to delete task");
+  return res.json();
+};
